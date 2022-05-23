4f353c6779a0ce17d5a734216327abfd5fa6d4be
Lucas Alves <lucasalvesf78@gmail.com>
2022-05-23 16:39:47 -0300

    v1.0

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import GoogleLogin from "react-google-login";
+
+function App() {
+	const [name, setName] = useState();
+	const [email, setEmail] = useState();
+	const [profilePic, setProfilePic] = useState();
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+	const responseGoogle = (response) => {
+		console.log(response);
+		const {
+			profileObj: { name, email, imageUrl },
+		} = response;
+		setName(name);
+		setEmail(email);
+		setProfilePic(imageUrl);
+		setIsLoggedIn(true);
+	};
+	return (
+		<div className="container">
+			<GoogleLogin
+				//substituir a proxima linha com o clientid do seu projeto
+				clientId="xxx"
+				buttonText="Continuar com o Google"
+				onSuccess={responseGoogle}
+				onFailure={responseGoogle}
+			/>
+			{isLoggedIn ? (
+				<div style={{ textAlign: "center" }}>
+					<h1>User Information</h1>
+					<img className="profile" src={profilePic} alt="Profile" />
+					<p>Name: {name}</p>
+					<p>Email: {email}</p>
+				</div>
+			) : (
+				""
+			)}
+		</div>
+	);
+}
+
+export default App;
